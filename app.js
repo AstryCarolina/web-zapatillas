05a28b6fec4fc1ec1bcad015d8b6b420e9bbfdad
Astry Carolina <112566622+AstryCarolina@users.noreply.github.com>
2023-05-17 23:16:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+const {body, validationResult} = require('express-validator')
+
+app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req, res)=>{
+    res.render('index')
+})
+
+app.post('/registrar', [
+    body('nya', 'Ingrese un nombre y apellido completo')
+        .exists()
+        .isLength({min:5}),
+    body('email', 'Ingrese un e-mail válido')
+        .exists()
+        .isEmail(),
+], (req, res) =>{
+
+    /* const errors = validationResult(req)
+    if (!errors. isEmpty()) {
+        res.status(400).json({errors: errors.array() });
+        console.log(errors)
+    } */
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        console.log(req.body)
+        const valores = req.body
+        const validaciones = errors.array()
+        res.render('index', {validaciones:validaciones, valores:valores})
+    }else{
+        res.send('¡Validación exitosa!')
+    }
+})
+
+app.listen(3000, () =>{
+    console.log('SERVER UP en http://localhost:3000')
+})
